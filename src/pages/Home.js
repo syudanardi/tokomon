f67a9562c2e596afdefa426353be7734c742062c
Surviantoro Ilham <surviantoro@gmail.com>
2021-04-28 02:46:30 +0700

    added version 1.0 to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ export const Home = () => {
     <React.Fragment>
       <div className="container mx-auto max-w-7xl mb-4">
         <h3 className="text-center text-3xl mt-2 text-base leading-8 text-black font-bold tracking-wide uppercase">
-          Pokemon Shopping App
+          Pokemon Shopping App V1.0
         </h3>
         <PokemonList/>
       </div>
